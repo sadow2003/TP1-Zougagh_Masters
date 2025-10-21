cbb6c2f5b7fa873f76041dd61ebebfd8f13202af
sadow2003 <mounsif.zougagh@gmail.com>
2025-10-21 19:41:33 +0100

    TP1_Zougagh_Mounsif

---
+++ src/main/java/ma/emsi/zougagh/tp0zougagh_mounsif/llm/LlmInteraction.java
@@ -0,0 +1,7 @@
+package ma.emsi.zougagh.tp0zougagh_mounsif.llm;
+
+public class LlmInteraction {
+    public LlmInteraction(String texteRequeteJson, String texteReponseJson, String s) {
+        
+    }
+}
